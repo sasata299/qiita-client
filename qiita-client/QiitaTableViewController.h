0e6cf2f165002b2f0d169487fbb84c155ac7859d
sasata299 <karaage299@gmail.com>
2014-05-14 13:11:36 +0900

    Introduce AFNetworking

---
+++ qiita-client/QiitaTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  QiitaTableViewController.h
+//  qiita-client
+//
+//  Created by sasata299 on 5/14/14.
+//  Copyright (c) 2014 sasata299. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QiitaTableViewController : UITableViewController
+
+@end
